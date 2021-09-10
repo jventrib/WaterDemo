a68d4f5e4c4a63a386d939d39c78ddf0d081888f
jventribout <jventribout@smartwavesa.com>
2021-09-10 14:30:49 +0200

    ok with Canvas, but very slow...

--- app/src/main/java/com/jventrib/waterdemo/MainActivity.kt
+++ app/src/main/java/com/jventrib/waterdemo/MainActivity.kt
@@ -4,6 +4,8 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.view.WindowMetrics
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
@@ -17,6 +19,10 @@ import androidx.compose.ui.graphics.asImageBitmap
 import androidx.core.graphics.set
 import com.jventrib.waterdemo.ui.theme.WaterDemoTheme
 import kotlinx.coroutines.android.awaitFrame
+import kotlinx.coroutines.delay
+import java.nio.Buffer
+import java.nio.IntBuffer
+import java.time.Instant
 import kotlin.random.Random
 
 
@@ -24,13 +30,17 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val background = BitmapFactory.decodeStream(resources.openRawResource(R.raw.stone_small))
-
         setContent {
             WaterDemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
 
-                    WaterBox(background)
+
+                    WaterBox(
+                        background,
+                        windowManager.currentWindowMetrics.bounds.width(),
+                        windowManager.currentWindowMetrics.bounds.height()
+                    )
                 }
             }
         }
@@ -38,9 +48,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun WaterBox(initialBackground: Bitmap, modifier: Modifier = Modifier) {
-    val width = initialBackground.width
-    val height = initialBackground.height
+fun WaterBox(initialBackground: Bitmap, width: Int, height: Int, modifier: Modifier = Modifier) {
+//    val width = initialBackground.width
+//    val height = initialBackground.height
+    val width = 400
+    val height = 300
+    val damping = 32
 
     fun Int.x(): Int {
         return this % width
@@ -50,37 +63,66 @@ fun WaterBox(initialBackground: Bitmap, modifier: Modifier = Modifier) {
         return this / width
     }
 
-    val buffer1 = IntArray(width * height)
-    val buffer2 = IntArray(width * height)
+    fun index(x: Int, y: Int) = y * width + x
+
+    val buffer1 = IntBuffer.allocate(width * height)
+    val buffer2 = IntBuffer.allocate(width * height)
     var currentBuffer by remember {
         mutableStateOf(buffer1, neverEqualPolicy())
     }
 
-    var ib = Bitmap.createBitmap(width, height, initialBackground.config)
+    var previousBuffer = buffer2
+
+    //    fun gridA(x: Int, y: Int) = (currentBuffer[index(x, y)])
+    fun gridA(x: Int, y: Int): Int {
+        val index = index(x, y)
+        return (
+                previousBuffer[index - 1]
+                        + previousBuffer[index + 1]
+                        + previousBuffer[index + width]
+                        + previousBuffer[index - width]
+                ) / 2 - currentBuffer[index]
+    }
+
+    var ib = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
 
     LaunchedEffect(Unit) {
         while (true) {
             awaitFrame()
-
-            currentBuffer.set(
-                Random.nextInt(0, 10) * width + Random.nextInt(0, 10),
-                Random.nextInt(255)
-            )
-            currentBuffer = currentBuffer
+            val start = System.currentTimeMillis()
+            currentBuffer.put(index(Random.nextInt(width), Random.nextInt(height)), 2000)
+            for (x in 1..width - 2) {
+                for (y in 1..height - 2) {
+                    val newValue = gridA(x, y)
+                    val damped = newValue - newValue / damping
+                    currentBuffer.put(index(x, y), damped)
+                }
+            }
+            // Swap buffer
+            val temp = currentBuffer
+            currentBuffer = previousBuffer
+            previousBuffer = temp
+            val end = System.currentTimeMillis()
+            Log.d("Water", "Frame time: ${end - start}ms")
         }
     }
     Box {
         Canvas(Modifier.fillMaxSize()) {
-            for (index in 0 until currentBuffer.size) {
-                val i = currentBuffer[index]
-                ib[index.x(), index.y()] = Color.rgb(i, i, i)
+            for (index in 0 until currentBuffer.limit()) {
+                val i = (currentBuffer[index] + 123).max(255)
+                ib.set(index.x(), index.y(), Color.rgb(i, i, i))
             }
-
             drawImage(ib.asImageBitmap())
         }
     }
+}
 
-
+private fun Int.max(max: Int): Int {
+    return when {
+        this < 0 -> 0
+        this >= max -> max - 1
+        else -> this
+    }
 }
 
 
